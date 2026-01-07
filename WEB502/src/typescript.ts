f38e880c14c1d1f2f88243c2d5e430c2271984f6
kiemhieu-hub <nguyenhieu4mat@gmail.com>
2026-01-07 19:14:41 +0700

    các core trong typescr

--- WEB502/src/typescript.ts
+++ WEB502/src/typescript.ts
@@ -1,2 +1,51 @@
 let message: string = "Hello TypeScript";
-console.log(message);
+
+//1.core types: numm , string , boolean
+let myName: string = "noname"; //chuoi ki tu
+const age: number = 36;
+const isMarri:boolean = true;
+console.log(myName);
+
+
+//2. types inference suy dienex ra kieu dieu lieu
+let count = 10;
+// count = "20";// sai errror
+console.log(count);
+
+
+
+//3.cores types : object
+let product:{
+    id:number;
+    title:string;
+    cc:number;
+    isAs:boolean
+}={
+    id:1,
+    title:"ip11",
+    cc:22,
+    isAs:true
+}
+console.log(product);
+
+
+//4. cores types: array number[]
+let numbers: number[] = [1,2,3,4,"6"];
+let names: string[] = ["noname","abc","xyz"]
+
+//5. Special Types: tuple
+let tuple: [string, number] = ["Alice", 25];
+// Lỗi nếu sai kiểu hoặc độ dài
+console.log(tuple);
+
+
+//7. Type: Union và Literal Type
+//Union Type: Cho phép một biến có thể thuộc nhiều kiểu khác nhau.
+let result: number | string;
+result = 42; // Hợp lệ
+result = "Success"; // Hợp lệ
+result = true; // Lỗi
+//Literal Type: Chỉ định giá trị cụ thể mà biến có thể nhận.
+let status: "success" | "error" | "loading";
+status = "success"; // Hợp lệ
+status = "failed"; // Lỗi
